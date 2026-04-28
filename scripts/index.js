ef263960fe3ef61cf1ea56dcaa7343ce4a3138c1
SabbirReturn <mdsabbirhasan39@gmail.com>
2026-04-28 23:15:58 +0600

    again

--- scripts/index.js
+++ scripts/index.js
@@ -2,6 +2,7 @@ let loadData = async()=>{
     let res = await fetch('https://openapi.programming-hero.com/api/phones?search=iphone')
     let data = await res.json();
     let phones = data.data
+    // console.log(phones)
     console.log(phones)
 }
 loadData()
